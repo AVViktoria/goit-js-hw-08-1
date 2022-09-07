c1997abceff9017756533b97ea29511273d2ab37
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-07 01:19:12 -0700

    decoration

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
-
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// listStyle = document.querySelectorAll("ul");
+// listEl.style.cssText += 
+//   `list-style-type: none;`;
