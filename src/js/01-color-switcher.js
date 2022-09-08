42eed6df92aae16b68431f1972ea1279fa496318
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-07 23:26:43 -0700

    Task -1 Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,31 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// listStyle = document.querySelectorAll("ul");
-// listEl.style.cssText += 
-//   `list-style-type: none;`;
+const refs = {
+  bcgColor: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+refs.startButton.addEventListener('click', onClickChangeBcgColor);
+refs.stopButton.addEventListener('click', onClickStopBcgColor);
+let timerId = 0;
+
+function onClickChangeBcgColor(){
+ refs.startButton.disabled = true;
+ refs.stopButton.disabled = false;
+
+timerId = setInterval(() =>{
+  refs.bcgColor.style.background = getRandomHexColor();
+}, 1000);
+};
+
+function onClickStopBcgColor(){
+  refs.startButton.disabled = false;
+ refs.stopButton.disabled = true;
+  clearInterval(timerId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+  
+    
