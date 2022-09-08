26fd49fb247bfa3b3d9053aa8c6d3a7857255797
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-07 23:36:20 -0700

    Task-2 - question _ sections

---
+++ src/js/02-timer.js
@@ -0,0 +1,27 @@
+//*  Напиши скрипт таймера, 
+//*  который ведёт обратный 
+//*  отсчет до определенной даты. 
+//*  Такой таймер может использоваться 
+//*  в блогах и интернет-магазинах, 
+//*  страницах регистрации событий, 
+//*  во время технического обслуживания и т. д. 
+//*  Добавь минимальное оформление элементов интерфейса.
+//*  Используй библиотеку flatpickr для того 
+//*  чтобы позволить пользователю кроссбраузерно 
+//*  выбрать конечную дату и время в одном элементе интерфейса. 
+//*  Для того чтобы подключить CSS код библиотеки в проект, 
+//*  необходимо добавить еще один импорт, 
+//*  кроме того который описан в документации.
+ 
+// import flatpickr from "flatpickr";
+// // Дополнительный импорт стилей
+// import "flatpickr/dist/flatpickr.min.css";
+
+
+// const refs = {
+//   bcgColor: document.querySelector('body'),
+//   startButton: document.querySelector('[data-start]'),
+//   stopButton: document.querySelector('[data-stop]'),
+// };
+// refs.startButton.addEventListener('click', onClickChangeBcgColor);
+// refs.stopButton.addEventListener('click', onClickStopBcgColor);
