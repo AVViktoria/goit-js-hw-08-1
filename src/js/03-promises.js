907ae5bf7dd0a31f7383f38a27bb3d3e4afc50a5
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-12 14:29:08 -0700

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,51 +1,42 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const refs = {
   form: document.querySelector('.form'),
-  // delay: document.querySelector('[name="delay"]'),
-  // step: document.querySelector('[name="step"]'),
-  // amount: document.querySelector('[name="amount"]'),
-  // button: document.querySelector('[type="submit"]'),
 };
 refs.form.addEventListener('submit', createPromptSubmit);
 
-
-function createPromptSubmit(e){
+function createPromptSubmit(e) {
   e.preventDefault();
-  delayValue = Number(e.currentTarget.delay.value);
- amountValue = Number(e.currentTarget.amount.value);
-  stepValue = Number(e.currentTarget.step.value);
+  let delayValue = Number(e.currentTarget.delay.value);
+  let amountValue = Number(e.currentTarget.amount.value);
+  let stepValue = Number(e.currentTarget.step.value);
 
-
-  for(let i=1; i<=amountValue; i+=1){
+  for (let i = 1; i <= amountValue; i += 1) {
     createPromise(i, delayValue)
-  .then(({ position, delay }) => {
-setTimeout(() => {
-  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-}, delay);
-    
-  })
-  .catch(({ position, delay }) => {
-    setTimeout(()=> {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    }, delay);
-    
-  });
-  delayValue +=stepValue;
-  e.target.reset();
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delayValue += stepValue;
+    e.target.reset();
   }
 }
 
-
 function createPromise(position, delay) {
-const shouldResolve = Math.random() > 0.3; 
-const objectPromise = {position, delay};
+  const shouldResolve = Math.random() > 0.3;
+  const objectPromise = { position, delay };
 
-  return new Promise((resolve, reject) =>{
-  if(shouldResolve){
-      resolve(objectPromise);
-    }
+  return new Promise(
+    (resolve, reject) => {
+      if (shouldResolve) {
+        resolve(objectPromise);
+      }
       reject(objectPromise);
     }
-    // , delay
-    )
-  }
+  );
+}
